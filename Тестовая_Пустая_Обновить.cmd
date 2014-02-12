33a4b22564fc40c0a0473ede40650207465d16a4
artbear <aartbear@gmail.com>
2014-02-13 00:36:49 +0400

    .

--- Тестовая_Пустая_Обновить.cmd
+++ Тестовая_Пустая_Обновить.cmd
@@ -1,3 +1,3 @@
 @echo off
-"C:\Windows\SysWOW64\cscript.exe" W:\Конфигурации\Связной\Admin1C\confUtils.vbs W:\Конфигурации\Связной\Admin1C\confupdate_empty1.ini
+"C:\Windows\SysWOW64\cscript.exe" W:\Конфигурации\Связной\Admin1C\confUtils.vbs W:\Конфигурации\Связной\Admin1C\confupdate_empty.ini
 pause
