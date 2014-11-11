2d0a36d734d23c4546da48911e06ed76176d3c41
Артур Аюханов aka artbear <aartbear@gmail.com>
2014-11-11 14:06:45 +0300

    Убрал лишние файлы от чужого проекта

--- Тестовая_Пустая_Обновить.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-"C:\Windows\SysWOW64\cscript.exe" W:\Конфигурации\Связной\Admin1C\confUtils.vbs W:\Конфигурации\Связной\Admin1C\confupdate_empty.ini
-pause
--- ТестоваяОбщая_Обновить_НеТерминал.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-"C:\Windows\SysWOW64\cscript.exe" \\WorkServer\Share\Admin1C\confUtils.vbs \\WorkServer\Share\Admin1C\confupdate_IM_test.ini
-pause
